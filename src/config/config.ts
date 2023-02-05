1e24b4a34572b1543e828af2a1c183622574ba18
Tobias Wälde <tobias.waelde@gmail.com>
2023-02-05 04:57:38 +0100

    add configuration keys

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+export type ConfigurationKeys =
+	| 'apiKey'
+	| 'fetchLimit'
+	| 'tracking.workspaceId'
+	| 'tracking.projectId'
+	| 'tracking.taskId'
+	| 'tracking.tagIds'
+	| 'tracking.billable'
+	| 'tracking.autostart'
+	| 'tracking.autostop';
