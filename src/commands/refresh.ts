da4050e7c1c22485c5fcd6330a36bcd79f22dd43
Tobias Wälde <tobias.waelde@gmail.com>
2023-02-18 02:15:56 +0100

    add command to refresh all

--- src/commands/refresh.ts
+++ src/commands/refresh.ts
@@ -1,5 +1,6 @@
 import { refreshClients } from '../views/treeview/clients/commands/refresh-clients';
 import { refreshProjects } from '../views/treeview/projects/commands/refresh-projects';
+import { refreshTags } from '../views/treeview/tags/commands/refresh-tags';
 import { refreshTasks } from '../views/treeview/tasks/commands/refresh-tasks';
 import { refreshWorkspaces } from '../views/treeview/workspaces/commands/refresh-workspaces';
 
@@ -8,4 +9,5 @@ export function refresh() {
 	refreshClients();
 	refreshProjects();
 	refreshTasks();
+	refreshTags();
 }
