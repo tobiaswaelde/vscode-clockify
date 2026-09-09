2fa49fc8565d804b0e24a803f6ca7388ddcc95a5
Tobias Wälde <t.waelde@pm.me>
2026-09-09 16:17:44 +0200

    Implement project delete command (#71)

---
+++ .changeset/quiet-projects-leave.md
@@ -0,0 +1,5 @@
+---
+"clockify-tracker": patch
+---
+
+Implement confirmed project deletion from the Projects tree-view context menu.
--- src/views/treeview/projects/commands/delete-project.ts
+++ src/views/treeview/projects/commands/delete-project.ts
@@ -1,9 +1,31 @@
 import { window } from 'vscode';
 import { TreeView } from '../..';
+import { Clockify } from '../../../../sdk';
+import { Workspace } from '../../../../sdk/types/workspace';
+import { showError } from '../../../../sdk/util';
+import { Dialogs } from '../../../../util/dialogs';
+import { GlobalState } from '../../../../util/global-state';
 import { ProjectItem } from '../items/item';
 
-export async function deleteProject(element: ProjectItem): Promise<void> {
-	window.showErrorMessage('Not yet implemented.');
+export async function deleteProject(element?: ProjectItem): Promise<void> {
+	const workspace = GlobalState.get<Workspace>('selectedWorkspace');
+	if (!workspace || !element?.project) {
+		return showError('No workspace or project selected.');
+	}
+
+	const confirmation = await Dialogs.askForConfirmation(
+		`Do you really want to delete project '${element.project.name}'?`
+	);
+	if (confirmation !== 'Yes') {
+		return;
+	}
+
+	const deletedProject = await Clockify.deleteProject(workspace.id, element.project.id);
+	if (!deletedProject) {
+		return;
+	}
+
+	window.showInformationMessage(`Project '${deletedProject.name}' deleted.`);
 	TreeView.refreshProjects();
 	TreeView.refreshTasks();
 	TreeView.refreshTimeentries();
